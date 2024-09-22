aac831a1f16ac87e1b27c6e61be6285062495cac
alex-shapovalov <shapovalov.alex@pm.me>
2024-09-21 21:04:08 -0400

    Attempt at sending query to firestore

--- admin/config.py
+++ admin/config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 def init():
-    cred = credentials.Certificate("./cereal-64ada-firebase-adminsdk-mksnj-049fd500df.json")
+    cred = credentials.Certificate("../admin/cereal-64ada-firebase-adminsdk-mksnj-049fd500df.json")
     firebase_admin.initialize_app(cred)
 
     return firestore.client()
--- query/query.py
+++ query/query.py
@@ -1,43 +1,31 @@
-from ..admin.config import init
+from admin.config import init
+from google.cloud.firestore_v1.base_query import FieldFilter
 
-def query_parser(dictionary):
-    # Remove this later, testing to see if function is receiving info correctly
-    print(f"Received Dictionary: {dictionary}")
+def query_parser(query_list):
+    print(f"Received Query List: {query_list}")
 
+    # Initialize database
     cereal_database = init()
     print(f"Cereal Database Initialized: {cereal_database}")
 
-    # 1. Take the dictionary and assign variables to various fields:
     try:
-        calories = dictionary.get('calories')
-        cups = dictionary.get('cups')
-        fiber = dictionary.get('fiber')
-        manufacturer = dictionary.get('manufacturer')
-        sugars = dictionary.get('sugars')
-        rating = dictionary.get('rating')
+        # Access database collection
+        query_ref = cereal_database.collection('Cereal')
 
-        # Verifying fields received correctly
-        print(f"calories: {calories}\n"
-              f"cups: {cups}\n"
-              f"fiber: {fiber}\n"
-              f"manufacturer: {manufacturer}\n"
-              f"sugars: {sugars}\n"
-              f"rating: {rating}")
+        # For each dictionary in our query_list, grab attribute, operator, and number_name
+        # Create query and push it to firestore
+        for query in query_list:
+            attribute = query.get('attribute')
+            operator = query.get('operator')
+            number_name = query.get('number_name')
 
-    # 2. Send to firestore in the following format and print:
+            # Need to handle 'and' queries here
 
-        # Originally had this line below which also matched what you had, ...
-            #parsed_query = cereal_database.where(calories, cups, fiber, manufacturer, sugars, rating)
-        # but external source said .where needs 3 arguments (field_path, op_string, value)
-        # So I edited it. Not sure if we need to specify the collection or not
-        parsed_query = cereal_database.collection('Cereal') \
-            .where('calories', '==', calories).where('cups', '==', cups) \
-            .where('fiber', '==', fiber).where('manufacturer', '==', manufacturer) \
-            .where('sugars', '==', sugars).where('rating', '==', rating)
-        query_stream = parsed_query.stream()
+            # Send completed_query to firestore, currently holding program infinitely
+            completed_query = query_ref.where(filter=FieldFilter(attribute, operator, number_name))
+            docs = completed_query.get()
 
-        for query in query_stream:
-            print(f'{query.id} => {query.to_dict()}')
+            print(str(docs))
 
     except Exception as error:
         print(f"Error: {error}")
