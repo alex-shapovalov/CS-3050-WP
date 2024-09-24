9bed1d1481640e1946863cf0a27d7f51f9f31880
alex-shapovalov <shapovalov.alex@pm.me>
2024-09-24 14:50:45 -0400

    Code should work for handling singular and multiple queries, still freezing on Firestore

--- query/query.py
+++ query/query.py
@@ -1,5 +1,44 @@
 from admin.config import init
-from google.cloud.firestore_v1.base_query import FieldFilter
+from google.cloud.firestore_v1.base_query import FieldFilter, BaseCompositeFilter
+from google.cloud.firestore_v1.types import StructuredQuery
+
+def build_query(query_ref, query_list):
+    # If there is only one condition in the query list, process normally
+    if len(query_list) == 1:
+        query = query_list[0]
+        attribute = query.get('attribute')
+        operator = query.get('operator')
+        number_name = query.get('number_name')
+
+        print(f"Processing query: {attribute} {operator} {number_name}")
+
+        field_filter = FieldFilter(attribute, operator, float(number_name))
+
+        return query_ref.where(filter=field_filter)
+
+    # If there are multiple conditions, tie them together with AND's
+    else:
+        filters = []
+
+        for query in query_list:
+            attribute = query.get('attribute')
+            operator = query.get('operator')
+            number_name = query.get('number_name')
+
+            print(f"Processing query: {attribute} {operator} {number_name}")
+
+            # Create a FieldFilter for each query condition and add it to the list
+            field_filter = FieldFilter(attribute, operator, float(number_name))
+            filters.append(field_filter)
+
+            # Combine with AND's
+            # https://github.com/googleapis/python-firestore/issues/705
+            composite_filter = BaseCompositeFilter(
+                operator=StructuredQuery.CompositeFilter.Operator.AND,
+                filters=filters
+            )
+
+            return query_ref.where(filter=composite_filter)
 
 def query_parser(query_list):
     print(f"Received Query List: {query_list}")
@@ -12,28 +51,10 @@ def query_parser(query_list):
         # Access database collection
         query_ref = cereal_database.collection('Cereal')
 
-        count = 0
+        completed_query = build_query(query_ref, query_list)
 
-        # For each dictionary in our query_list, grab attribute, operator, and number_name
-        # Create query and push it to firestore
-        for query in query_list:
-            attribute = query.get('attribute')
-            operator = query.get('operator')
-            number_name = query.get('number_name')
-
-            # Send completed_query to firestore, currently holding program infinitely
-            completed_query = query_ref.where(filter=FieldFilter(attribute, operator, number_name))
-            docs = completed_query.get()
-
-            print(str(docs))
-
-            # Need to handle 'and' queries here, maybe add 'and' if looping
-            # through more than once
-
-            # if count > 1:
-            #     concat 'and' at end of the query
-
-            count = count + 1
+        docs = completed_query.get()
+        print(str(docs))
 
     except Exception as error:
         print(f"Error: {error}")
