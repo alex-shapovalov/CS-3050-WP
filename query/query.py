9574e9e9618bace7d0d4772d6c7ee75e435855b0
alex-shapovalov <shapovalov.alex@pm.me>
2024-09-25 15:15:41 -0400

    Almost done

--- query/query.py
+++ query/query.py
@@ -2,7 +2,7 @@ from google.cloud.firestore_v1.base_query import FieldFilter, BaseCompositeFilte
 from google.cloud.firestore_v1.types import StructuredQuery
 
 def query_parser(query_list, cereal_database):
-    try:
+    # try:
         # Access database collection
         query_ref = cereal_database.collection('Cereal')
         print(str(query_list))
@@ -25,44 +25,43 @@ def query_parser(query_list, cereal_database):
                 pass
 
         # If there are multiple conditions, tie them together with AND's
+        if len(query_list) == 2:
+            query_one = query_list[0]
+            attribute_one = query_one.get('attribute')
+            operator_one = query_one.get('operator')
+            number_name_one = query_one.get('number_name')
+            return_value_one = query_one.get('return_value')
+
+            query_two = query_list[1]
+            attribute_two = query_two.get('attribute')
+            operator_two = query_two.get('operator')
+            number_name_two = query_two.get('number_name')
+            return_value_two = query_two.get('return_value')
+
+            field_filter_one = FieldFilter(attribute_one, operator_one, number_name_one)
+            field_filter_two = FieldFilter(attribute_two, operator_two, number_name_two)
+
+            if attribute_one == "name" and attribute_two == "name":
+                completed_query = query_ref.where(filter=field_filter_one).select([return_value_one]).where(filter=field_filter_two).select([return_value_two])
+
+            elif attribute_one == "name" and attribute_two != "name":
+                completed_query = query_ref.where(filter=field_filter_one).select([return_value_one]).where(filter=field_filter_two)
+
+            elif attribute_one != "name" and attribute_two == "name":
+                completed_query = query_ref.where(filter=field_filter_one).where(filter=field_filter_two).select([return_value_two])
+
+            else:
+                completed_query = query_ref.where(filter=field_filter_one).where(filter=field_filter_two)
+
+            # "of" queries must be handled differently
+
         else:
-            return_value_map = {}
-            completed_query = query_ref
-
-            def recursive_and(completed_query, index):
-                # Base case: If index reaches the end of query_list, return the completed query
-                if index >= len(query_list):
-                    return completed_query
-
-                # Get the current query item
-                query = query_list[index]
-                attribute = query.get('attribute')
-                operator = query.get('operator')
-                number_name = query.get('number_name')
-                return_value = query.get('return_value')
-
-                # Store every instance of "of" queries
-                if attribute == "name":
-                    return_value_map[number_name] = return_value
-
-                field_filter = FieldFilter(attribute, operator, number_name)
-
-                # Chain together where for "and"
-                completed_query = query_ref.where(filter=field_filter)
-
-                return recursive_and(completed_query, index + 1)
-
-            completed_query = recursive_and(completed_query, 0)
-
-            # Once recursion is done, apply select() if there is a 'name' query
-            if return_value_map:
-                of = list(return_value_map.values())
-                completed_query = completed_query.select(of)
+            print("Error: Query contains two many elements (probably using more than 1 \'AND\')")
 
         docs = completed_query.get()
 
         for doc in docs:
             print(doc.id, doc.to_dict())
 
-    except Exception as error:
-        print(f"Error: {error}")
+    # except Exception as error:
+    #     print(f"Error: {error}")
