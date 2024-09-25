39fff2b2d22b2f422258ee66abefad5a46351a35
alex-shapovalov <shapovalov.alex@pm.me>
2024-09-25 14:13:45 -0400

    Almost done

--- query/query.py
+++ query/query.py
@@ -1,59 +1,66 @@
 from google.cloud.firestore_v1.base_query import FieldFilter, BaseCompositeFilter
 from google.cloud.firestore_v1.types import StructuredQuery
 
-database = False
-
-def build_query(query_ref, query_list):
-    # If there is only one condition in the query list, process normally
-    if len(query_list) == 1:
-        query = query_list[0]
-        attribute = query.get('attribute')
-        operator = query.get('operator')
-        number_name = query.get('number_name')
-
-        print(f"Processing query: {attribute} {operator} {number_name}")
-
-        field_filter = FieldFilter(attribute, operator, number_name)
-
-        return query_ref.where(filter=field_filter)
-
-    # If there are multiple conditions, tie them together with AND's
-    else:
-        filters = []
-
-        for query in query_list:
-            attribute = query.get('attribute')
-            operator = query.get('operator')
-            number_name = query.get('number_name')
-
-            print(f"Processing query: {attribute} {operator} {number_name}")
-
-            # Create a FieldFilter for each query condition and add it to the list
-            field_filter = FieldFilter(attribute, operator, float(number_name))
-            filters.append(field_filter)
-
-            # Combine with AND's
-            # https://github.com/googleapis/python-firestore/issues/705
-            composite_filter = BaseCompositeFilter(
-                operator=StructuredQuery.CompositeFilter.Operator.AND,
-                filters=filters
-            )
-
-            return query_ref.where(filter=composite_filter)
-
 def query_parser(query_list, cereal_database):
-    print(f"Received Query List: {query_list}")
-
     try:
         # Access database collection
         query_ref = cereal_database.collection('Cereal')
+        print(str(query_list))
 
-        completed_query = build_query(query_ref, query_list)
+        if len(query_list) == 1:
+            query = query_list[0]
+            attribute = query.get('attribute')
+            operator = query.get('operator')
+            number_name = query.get('number_name')
+            return_value = query.get('return_value')
+
+            field_filter = FieldFilter(attribute, operator, number_name)
+
+            completed_query = query_ref.where(filter=field_filter)
+
+            # "of" queries must be handled differently
+            if attribute == "name":
+                completed_query = completed_query.select([return_value])
+            else:
+                pass
+
+        # If there are multiple conditions, tie them together with AND's
+        else:
+            return_value_map = {}
+            completed_query = query_ref
+
+            def recursive_and(completed_query, index):
+                # Base case: If index reaches the end of query_list, return the completed query
+                if index >= len(query_list):
+                    return completed_query
+
+                # Get the current query item
+                query = query_list[index]
+                attribute = query.get('attribute')
+                operator = query.get('operator')
+                number_name = query.get('number_name')
+                return_value = query.get('return_value')
+
+                # Store every instance of "of" queries
+                if attribute == "name":
+                    return_value_map[number_name] = return_value
+
+                field_filter = FieldFilter(attribute, operator, number_name)
+
+                # Chain together where for "and"
+                completed_query = query_ref.where(filter=field_filter)
+
+                return recursive_and(completed_query, index + 1)
+
+            completed_query = recursive_and(completed_query, 0)
+
+            # Once recursion is done, apply select() if there is a 'name' query
+            if return_value_map:
+                of = list(return_value_map.values())
+                completed_query = completed_query.select(of)
 
         docs = completed_query.get()
-        print(str(docs))
 
-        # Print the query results
         for doc in docs:
             print(doc.id, doc.to_dict())
 
